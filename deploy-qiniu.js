d91a9cfa835f79ae0d29e9dc53fb4ad7942b46b3
Nimo <nimo.jser@gmail.com>
2017-09-19 18:27:25 +0800

    open url

--- deploy-qiniu.js
+++ deploy-qiniu.js
@@ -1,3 +1,4 @@
+var opn = require('opn')
 var domain = '' || process.env.EDM_QINIU_DOMAIN
 fis.match('**', {
     domain: domain,
@@ -7,4 +8,21 @@ fis.match('**', {
         bucket: 'edm-fis' || process.env.EDM_QINIU_BUCKET
     })
 })
-console.log(domain)
+fis.match('**.html', {
+    parser: [
+        function (content, file) {
+            if (file.isHtmlLike) {
+                if (/index_.*\.html$/.test(file.subpath)) {
+                    var url = domain + file.subpath
+                    var commentsURL = '<!--EDM_URL_INFO-- ' + url + ' -->'
+                    console.log(url)
+                    opn(url)
+                    if (!/EDM_URL_INFO/.test(content)) {
+                        content = content.replace(/<\/body>/, commentsURL + '</body>')
+                    }
+                }
+            }
+            return content
+        }
+    ]
+})
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var livereload = require('livereload');
 var lrserver = livereload.createServer();
+var opn = require('opn');
 var express = require('express')
+var serveIndex = require('serve-index')
 lrserver.watch(__dirname + "/output");
+var port = 2411
 var app = express()
+app.use(
+    '/',
+    serveIndex(
+        'output',
+        {
+            'icons': true
+        }
+    )
+)
 app.use(express.static('output'))
-app.listen(3000)
-console.log('http://127.0.0.1:3000')
+app.listen(port)
+var url = 'http://127.0.0.1:' + port
+console.log(url)
+opn(url)
